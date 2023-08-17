d23d4a554a909d8376dea72b5c5ab4bc93bf0b4d
Milad Norouzi <miladnorouzi@icloud.com>
2023-08-17 13:40:25 +0330

    ver 1.0.1 upload

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = 'A LoRa commiunication package'
 LONG_DESCRIPTION = 'This library is for LoRa modules (Sx127x) based on the Python language'
 
 setup(
-    name="convsn",
+    name="LoRathon",
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author="Milad Norouzi",
-    author_email="miladnorouzi@icloud.com",
+    author_email="miladnorouzi1998@gmail.com",
     license='GPL-3.0',
+    url='https://github.com/miladnorouziii/LoRathon',
     packages=find_packages(),
-    install_requires=[],
     keywords='Lora',
+    install_requires=['spidev>=1.6',
+                      'RPi.GPIO'                     
+                      ],
     classifiers= [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        'License :: OSI Approved :: GPL-3.0 License',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         "Programming Language :: Python :: 3",
     ]
 )
